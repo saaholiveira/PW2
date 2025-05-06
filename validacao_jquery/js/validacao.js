2b365fc1c6d1c65e0d74320ad789b3dd4d0e2af0
Sarah Oliveira <167816807+saaholiveira@users.noreply.github.com>
2025-05-06 14:22:36 -0300

    Update validacao.js

--- validacao_jquery/js/validacao.js
+++ validacao_jquery/js/validacao.js
@@ -1,34 +1,57 @@
 
-$("#formExemplo").valiate({
+$("#formExemplo").validate({
     debug: true,
-    ruls:{
+    rules: {
         txtemail: {
             required: true,
-            email: true,
+            email: true
         },
         txtnome: {
             required: true,
-            rangelength: [2, 50],
+            rangelength: [2, 50]
         },
-        txtidade: {
-            min: 18,
-            max: 100,
-            required: true,
+        txtfilhos: {
+            min: 0,
+            max: 10,
+            required: true
+        },
+        txtsalario: {
+            min: 2000,
+            max: 10000,
+            required: true
+        },
+        txtadmissao: {
+            required: true
+        },
+        txtredesocial: {
+            required: true
+        }
     },
-    messages:{
+    messages: {
         txtemail: {
             required: "Campo obrigatório",
-            email: "Email inválido",
+            email: "Email inválido"
         },
         txtidade: {
-            min: "Idade mínima 18 anos",
-            max: "Idade máxima 100 anos",
-            required: "Campo obrigatório",
+            min: "mínimo de filhos 0",
+            max: "Máximo de gilhos 10",
+            required: "Campo obrigatório"
         },
         txtnome: {
             required: "Nome obrigatório",
             minlength: "Exigido 2 caracteres",
-            maxlength: "Até 50 caracteres",
+            maxlength: "Até 50 caracteres"
+        },
+        txtsalario: {
+            min: "Salário mínimo de R$2000",
+            max: "Salário Máximo de R$10000",
+            required: "Salário obrigatório"
+        },
+        txtadmissao: {
+            required: "Data de admissão obrigatório"
+        },
+        txtredesocial: {
+            required: "Rede social obrigatória"
         }
     }
 });
